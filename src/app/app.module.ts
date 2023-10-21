ac3c60bbc7e9959138f91aacbecbb835e5c90e89
Thisgo.Mts100 <martins734@Gmail.com>
2023-10-21 20:59:14 -0300

    components createds with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './componets/menu-bar/menu-bar.component';
+import { MenuTittleComponent } from './componets/menu-tittle/menu-tittle.component';
+import { BigCardComponent } from './componets/big-card/big-card.component';
+import { SmallCardComponent } from './componets/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTittleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componets/menu-tittle/menu-tittle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tittle',
+  templateUrl: './menu-tittle.component.html',
+  styleUrls: ['./menu-tittle.component.css']
+})
+export class MenuTittleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
